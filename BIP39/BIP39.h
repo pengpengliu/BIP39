82973133cef77680cc00ac4f7b599a5b36ce03fd
Liu Pengpeng <liupppppp@gmail.com>
2019-08-23 10:50:34 +0800

    Bump version to 1.0.0

---
+++ BIP39/BIP39.h
@@ -0,0 +1,19 @@
+//
+//  BIP39.h
+//  BIP39
+//
+//  Created by Liu Pengpeng on 2019/8/22.
+//  Copyright © 2019 Liu Pengpeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BIP39.
+FOUNDATION_EXPORT double BIP39VersionNumber;
+
+//! Project version string for BIP39.
+FOUNDATION_EXPORT const unsigned char BIP39VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BIP39/PublicHeader.h>
+
+
